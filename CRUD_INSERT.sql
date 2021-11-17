79f0422d14fe6142ec5e1798c103b1e02b62cb2c
Ananya Goel <88566978+ananya555@users.noreply.github.com>
2021-11-17 14:18:23 +0530

    Update CRUD_INSERT.sql

--- CRUD_INSERT.sql
+++ CRUD_INSERT.sql
@@ -11,13 +11,15 @@ CREATE TABLE employee(
 );
 INSERT INTO employee (f_name,m_name,l_name,age) VALUES ('Ram','Kumar', 'Sharma', 35);
 SELECT* FROM employee;
-//ALWAYS PUT VARCHAR VALUES IN SINGLE QUOTES
+/*ALWAYS PUT VARCHAR VALUES IN SINGLE QUOTES*/
 
 INSERT INTO employee (f_name,m_name,l_name,age) VALUES ('RaNI','KumarI', 'PATEL', 30), ('HONEY','BUNNY', 'SUNNY', 40),('Preta','Dhillon','Bisht',26);
 
 INSERT INTO employee(f_name, l_name, age, curr_status) VALUES('sakshi','yadav',22,'employed'),('shivangi','singh',28, 'fired'),('deepti', 'bharti', 26,'fired'),('kushbu', 'singh',29,'employed'),('shikha','sharma',21, 'intern'),('satakshi','aithani',20,'intern'),('dular', 'roy',25,'part-time employee');
 INSERT INTO employee(f_name,m_name, l_name, age, curr_status) VALUES ('jigyasa','kumari','singh',30,'part-time employee'),('santa','san','ben',33, 'fired'), ('divya','kaur','singh',23, 'intern');
-//ALWAYS REMEMBER YOU CANNOT WRITE A FILED_NAME & SKIP IT IN THR VALUES_PORTION THO U CAN IGNORE FIELD_NAME IN THE DECLARATION PART
+
+
+/*ALWAYS REMEMBER YOU CANNOT WRITE A FILED_NAME & SKIP IT IN THR VALUES_PORTION THO U CAN IGNORE FIELD_NAME IN THE DECLARATION PART*/
 
 INSERT INTO employee(f_name,m_name, l_name, age, curr_status) VALUES('helena',,'roy',33,'fired');
 
